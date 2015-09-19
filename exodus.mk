789fe1aa5f865331e0054555ca130b6161bf8c63
Raja Mungamuri <Raja1612.M@gmail.com>
2015-09-19 16:45:59 +0530

    Adding support file of EXODUS-5.1 for serranoltexx device
    samsung galaxy s4 mini

--- exodus.mk
+++ exodus.mk
@@ -1,8 +1,8 @@
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/exodus/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/exodus/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/samsung/serranoltexx/full_serranoltexx.mk)
 
